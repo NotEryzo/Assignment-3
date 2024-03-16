c040faf4ee4c3f04868529577db040a657322e4d
NotEryzo <119642792+NotEryzo@users.noreply.github.com>
2024-03-16 16:01:21 -0600

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,126 @@
+class BoundedQueue: 
+    # Creates a new empty queue:
+    def __init__(self, capacity): 
+        assert isinstance(capacity, int), ('Error: Type error: %s' % (type(capacity))) # throws an assertion error on not true
+        assert capacity >= 0, ('Error: Illegal capacity: %d' % (capacity))
+        self.__items = [] # init the  list / queue as empty
+        self.__capacity = capacity
+ 
+    # Adds a new item to the back of the queue, and returns nothing:
+    def enqueue(self, item): 
+        '''
+        Enqueue the element to the back of the queue
+        :param item: the element to be enqueued
+        :return: No returns
+        Refrences: Lecture Slides
+        '''
+        if len(self.__items)>=self.__capacity:
+            raise Exception("Error: Queue is full")
+        '''
+        Remember to check the conditions
+        '''
+        self.__items.append(item)
+        
+    # Removes and returns the front-most item in the queue.      
+    # Returns nothing if the queue is empty.    
+    def dequeue(self):        
+        '''
+        Dequeue the element from the front of the queue and return it
+        :return: The object that was dequeued
+        Refrences: Lecture Slides
+        '''
+        if len(self.__items) <= 0:
+            raise Exception("Error: Queue is empty")
+        '''
+        1. remember to check the conditions
+        2. return the appropriate value
+        '''
+        return self.__items.pop(0)
+    
+    # Returns the front-most item in the queue, and DOES NOT change the queue.      
+    def peek(self):        
+        if len(self.__items) <= 0:            
+            raise Exception('Error: Queue is empty')        
+        return self.__items[0]
+        
+    # Returns True if the queue is empty, and False otherwise:    
+    def isEmpty(self):
+        return len(self.__items) == 0        
+    
+    # Returns True if the queue is full, and False otherwise:    
+    def isFull(self):
+        return len(self.__items) == self.__capacity
+    
+    # Returns the number of items in the queue:    
+    def size(self):        
+        return len(self.__items)        
+    
+    # Returns the capacity of the queue:    
+    def capacity(self):        
+        return self.__capacity
+    
+    # Removes all items from the queue, and sets the size to 0    
+    # clear() should not change the capacity    
+    def clear(self):        
+        self.__items = []
+
+    # Returns a string representation of the queue: 
+    def __str__(self):               
+        str_exp = ""        
+        for item in self.__items:            
+            str_exp += (str(item) + " ")                    
+        return str_exp
+        
+    # Returns a string representation of the object bounded queue: 
+    def __repr__(self):               
+        return  str(self) + " Max=" + str(self.__capacity)
+    
+class BoundedStack:
+    def __init__(self, capacity):
+        assert isinstance(capacity, int), ('Error: Type error: %s' % (type(capacity))) # throws an assertion error on not true
+        assert capacity >= 0, ('Error: Illegal capacity: %d' % (capacity))
+        self.items = []
+        self.capacity = capacity
+
+    def push(self, item):
+        if len(self.items) >= self.capacity:
+            raise Exception("Error: Stack is full")
+        self.items.append(item)
+
+    def pop(self):
+        if self.isEmpty():
+            raise Exception("Error: Stack is empty")
+        return self.items.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            raise Exception("Error: Stack is empty")
+        return self.items[-1]
+
+    def isEmpty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+
+    def show(self):
+        print(self.items)
+
+    def clear(self):
+        self.items = []
+
+    def __str__(self):
+        stackAsString = ''
+        for item in self.items:
+            stackAsString += str(item) + ' '
+        return stackAsString
+
+    def __repr__(self):
+        return str(self) + " Max=" + str(self.capacity)
+    
+def openFile(filename):
+    with open(filename, "r") as file:
+        file = file.read().strip().split()
+        print(file)
+
+openFile("chemicals.txt")
